4d547c9f59a6363a5e44c2bc1df815dc36089976
blasey <justblasey@gmail.com>
2025-01-19 22:55:45 -0400

    adding file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 def uppercase(str):
     for c in str:
-        if 'a' <= c <= 'z':  # check if the character is lowercase
-            print(chr(ord(c) - 32), end="")  # convert lowercase to uppercase
-        else:
-            print(c, end="")  # print non-lowercase characters as is
-    print()  # ensure there's a newline at the end
+        print(chr(ord(c) - 32) if 'a' <= c <= 'z' else c, end="")
+    print()  # Ensure a new line is printed after the loop
