44b42a362b41226d9d03cc2f349a86163ddb0649
blasey <justblasey@gmail.com>
2025-01-19 23:24:22 -0400

    Adding files

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == '__main__':
-    """Prints the argument list passed to the program
-
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-
-    """
-    av = sys.argv
-    l_av = len(av) - 1
-
-    if l_av > 1:
-        print(l_av, 'arguments:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 1:
-        print(l_av, 'argument:')
-        for i in range(1, l_av + 1):
-            print('{:d}: {}'.format(i, av[i]))
-    elif l_av == 0:
-        print(l_av, 'arguments.')
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1  # Subtract 1 because the first argument is the script name
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
