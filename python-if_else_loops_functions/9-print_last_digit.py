fcbfc9acfcf3442f4352aecdac09b18e26c23280
blasey <justblasey@gmail.com>
2025-01-19 23:06:18 -0400

    Adding files

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python3
 def print_last_digit(number):
+    # Take the absolute value to handle negative numbers
     last_digit = abs(number) % 10
-    print(last_digit, end="")
+    
+    # Print the last digit
+    print(last_digit, end='')
+    
+    # Return the last digit
     return last_digit
