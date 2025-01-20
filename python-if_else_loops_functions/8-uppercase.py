352b564ac4093abbbf30e35e90212088e03b3a35
blasey <justblasey@gmail.com>
2025-01-19 22:53:33 -0400

    adding file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
             print(chr(ord(c) - 32), end="")
         else:
             print(c, end="")
