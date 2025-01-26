995d38b664242b93d561e96ae5bd58fab987cb87
blasey <justblasey@gmail.com>
2025-01-26 18:25:15 -0400

    Adding code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char not in ('c', 'C'):
-            new_string += char
-        return new_string
+    return ''.join([char for char in my_string if char not in ('c', 'C')])
