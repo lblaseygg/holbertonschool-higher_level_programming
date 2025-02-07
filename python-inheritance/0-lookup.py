9b1e6de43165f25c1413a3fbd613c381889c9008
blasey <justblasey@gmail.com>
2025-02-06 21:46:47 -0400

    Adding code

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+"""Module that provides a function to list available attributes and methods of an object."""
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object.
     
