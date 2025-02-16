780ebcb8f1420f09705770693bd26635b098fe72
blasey <justblasey@gmail.com>
2025-02-16 00:59:29 -0400

    Adding code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Defines a function that reads text file"""
+
+
 def read_file(filename=""):
-    """Reads a text file and prints it out to stdout."""
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Function returns a UTF* text file
+    Args:
+        filename: name of file to read
+    Return:
+        UTF8 text file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        read_file = f.read()
+        print(read_file, end="")
+    return read_file
