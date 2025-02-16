eb2d3a92b36cfed9f9f87a0fc15b00534593fa97
blasey <justblasey@gmail.com>
2025-02-16 01:00:23 -0400

    Adding code

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that provides a function to write a string to a text file."""
+
 def write_file(filename="", text=""):
     """Writes a string to a text file (UTF8) and returns the number of characters written."""
     with open(filename, "w", encoding="utf-8") as file:
